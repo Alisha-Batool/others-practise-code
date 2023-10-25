2d0f5d6e713dcf1ff3552c643629588adc56a778
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2023-10-25 03:48:20 -0700

    write code

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,9 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
     for(int num=0; num<=100; num++){
         if(num % 3==0 && num % 5==0){
-            print("FizzBuzz\n");
+            printf("FizzBuzz\n");
         }else if(num % 3==0){
             printf("Fizz\n");
         }else if(num % 5==0){
