2d78a3b1471c00f24dee306774a66cf40a6fe496
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2023-10-25 10:54:06 -0700

    include if condition

--- sqrt.c
+++ sqrt.c
@@ -7,14 +7,23 @@ int main()
 	printf("Enter the number whose square root is to be approximate: ");
 	scanf("%f", &number);
 	xPrev= guess;
-
+     
+     if (number < 0){
+        printf("Square root of a negative number is undefined.\n");
+        return 1;
+    }
+     
 	do
 	{
 		xCurrent= 0.5* (xPrev + number/xPrev);
 		calculatedError= fabs(xCurrent-xPrev);
 		xPrev= xCurrent;
+        if (xCurrent > errorRate) {
+            printf("Square root approximation exceeded maximum threshold.\n");
+            return 1;
+        }
 	}while(calculatedError>errorRate);
 	
-	printf("The square root approximated is %f\n ",xPrev,xCurrent);
+	printf("The square root approximated value is %f\n ",xPrev);
 	return 0;
 }
