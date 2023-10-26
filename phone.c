6012924f4a7e86627c261bdfa43ad27dbc381a7b
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2023-10-25 21:33:03 -0700

    EDIT AND WRITE CODE

---
+++ phone.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+int main()
+{
+    char token;
+    printf("enter the phone:");
+    scanf("%c", &token);
+    
+        
+        if(token=='A' || token=='B' || token=='C')
+        {
+            printf("2");
+           
+        }else if(token=='D' || token=='E' || token=='F')
+        {
+            printf("3");
+             
+        }else if(token=='G' || token=='H' || token=='I')
+        {
+            printf("4");
+             
+        }else if(token=='J' || token=='K' || token=='L')
+        {
+            printf("5");
+            
+        }else if(token=='M' || token=='N' || token=='O')
+        {
+            printf("6");
+             
+        }else if(token=='P' || token=='Q' || token=='R' || token=='S')
+        {
+            printf("7");
+            
+        }else if(token=='T' || token=='U' || token=='V' || token=='W')
+        {
+            printf("8");
+            
+        }else if(token=='x' || token=='Y' || token=='Z')
+        {
+            printf("9");
+            
+        }else{
+            printf("%c", &token);
+            return 0;
+        }
+    
+    return 0;
+}
