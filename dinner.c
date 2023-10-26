17629ade4407bcbf7a023af603b6186998bf1bf8
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2023-10-25 20:51:45 -0700

    some lodical errors

---
+++ dinner.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main()
+{
+    for(int x=0; x<=30; x++){
+        for(int y=0; y<=30; y++){
+            for(int z=0; z<=30; z++){
+                if ((3*x+2*y+z==50)&&(x+y+z==30))
+                {
+                    printf("%d gentlemen,%d ladies,%d children at the dinner.\n");
+                    break;
+                }
+            }
+        }
+    }
+    return 0;
+}
