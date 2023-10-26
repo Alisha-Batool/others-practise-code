ec61e34a16884ac7a0e915fd28811b7d2efe2b3c
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2023-10-25 21:40:29 -0700

    CHANGE braces place

--- phone.c
+++ phone.c
@@ -6,43 +6,51 @@ int main()
     printf("enter the phone:");
     scanf("%c", &token);
     
-        
+     do{   
         if(token=='A' || token=='B' || token=='C')
         {
             printf("2");
-           
-        }else if(token=='D' || token=='E' || token=='F')
+        }
+        else if(token=='D' || token=='E' || token=='F')
         {
             printf("3");
              
-        }else if(token=='G' || token=='H' || token=='I')
+        }
+        else if(token=='G' || token=='H' || token=='I')
         {
             printf("4");
              
-        }else if(token=='J' || token=='K' || token=='L')
+        }
+        else if(token=='J' || token=='K' || token=='L')
         {
             printf("5");
             
-        }else if(token=='M' || token=='N' || token=='O')
+        }
+        else if(token=='M' || token=='N' || token=='O')
         {
             printf("6");
              
-        }else if(token=='P' || token=='Q' || token=='R' || token=='S')
+        }
+        else if(token=='P' || token=='Q' || token=='R' || token=='S')
         {
             printf("7");
             
-        }else if(token=='T' || token=='U' || token=='V' || token=='W')
+        }
+        else if(token=='T' || token=='U' || token=='V' || token=='W')
         {
             printf("8");
             
-        }else if(token=='x' || token=='Y' || token=='Z')
+        }
+        else if(token=='x' || token=='Y' || token=='Z')
         {
             printf("9");
             
-        }else{
+        }
+        else
+        {
             printf("%c", &token);
             return 0;
         }
-    
+     }while(1);
     return 0;
 }
