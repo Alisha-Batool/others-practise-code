6a4448671d683b739606a9f5b3cf64c53f6361f3
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2023-10-27 10:36:11 -0700

    write code

--- pay.c
+++ pay.c
@@ -1,24 +1,21 @@
-#include <stdio.h>
-#include <math.h>
-
+#include<stdio.h>
 int main() {
-    float a, b, c;
-    float p, q;
-    float x1, x2;
+    int year;
 
-    printf("Enter the coefficients (a, b, c) of the quadratic equation: ");
-    scanf("%f %f %f", &a, &b, &c);
+    printf("Enter a year: ");
+    scanf("%d", &year);
 
-    p = -b / (2 * a);
-    q = sqrt(b * b - 4 * a * c) / (2 * a);
-
-    x1 = p + q;
-    x2 = p - q;
-
-    printf("The solutions to the quadratic equation are:\n");
-    printf("x1 = %.2f\n", x1);
-    printf("x2 = %.2f\n", x2);
+    if (year % 400 == 0) {
+        printf("%d is a leap year.\n", year);
+    } else if (year % 100 == 0) {
+        printf("%d is not a leap year.\n", year);
+    } else if (year % 4 == 0) {
+        printf("%d is a leap year.\n", year);
+    } else {
+        printf("%d is not a leap year.\n", year);
+    }
 
     return 0;
 }
+
 
