ca450afbda9fb9bb38fbc5293891381cecc5b500
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2023-10-27 07:48:23 -0700

    edit code

--- stars.c
+++ stars.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int starstoprint=3;
-    int spacestoprint=1;
+    int starstoprint = 3;
+    int spacestoprint = 1;
     for(int lines=1; lines<=7; lines++){
-        for(int stars = starstoprint; stars>0; stars--){
-            printf("*");
-        }
         for(int spaces = spacestoprint; spaces>0; spaces--){
             printf(" ");
-        }    
+        }
+        for(int stars = starstoprint; stars>0; stars--){
+            printf("*");
+        }   
+        //move cursor to the next line 
         printf("\n");
-
+        // update the stars and spaces
         if(lines>=4){
             starstoprint -= 2;
             spacestoprint++;
