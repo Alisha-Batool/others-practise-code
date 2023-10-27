2f91b889ba85f21d2abff673a9a0f6510a597424
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2023-10-27 07:44:45 -0700

    changes

--- stars.c
+++ stars.c
@@ -4,19 +4,19 @@ int main()
     int starstoprint=3;
     int spacestoprint=1;
     for(int lines=1; lines<=7; lines++){
-        for(int stars=starstoprint; stars>0; stars++){
+        for(int stars = starstoprint; stars>0; stars--){
             printf("*");
         }
-        for(int spaces=spacestoprint; spaces>0; spaces++){
+        for(int spaces = spacestoprint; spaces>0; spaces--){
             printf(" ");
         }    
         printf("\n");
 
         if(lines>=4){
-            starstoprint-=2;
+            starstoprint -= 2;
             spacestoprint++;
         }else{
-            starstoprint+=2;
+            starstoprint += 2;
             spacestoprint--;
         }
     }
