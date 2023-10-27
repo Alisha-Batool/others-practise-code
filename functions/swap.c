f961987dd1cd2955bbdabfd1cd1a3547d2db98d1
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2023-10-26 21:00:02 -0700

    logical changes

--- functions/swap.c
+++ functions/swap.c
@@ -2,15 +2,15 @@
 
  void swap( int a, int b);
  void swap( int a, int b) {
- int tmp = b;
- b = a; a = tmp;
+ int tmp = a;
+ a = b; b = tmp;
  return ;
 }
 
 int main()
 {
   
-int a = 5, b = 8;
+int a =5, b = 8;
 
  printf( "a = %d, b=%d \n" , a,b);
  swap(a , b) ;
