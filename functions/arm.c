806515fb61729338f7f6d29a64d32b86f58f85d3
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2023-10-26 22:34:32 -0700

    write code

--- functions/arm.c
+++ functions/arm.c
@@ -1,23 +1,44 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int isArmstrong(int number);
+int isArmstrong(int number) {
+    int originalNumber, remainder, result = 0, n = 0;
 
+    originalNumber = number;
 
+    // count the number of digits
+    while (originalNumber != 0) {
+        originalNumber /= 10;
+        ++n;
+    }
 
+    originalNumber = number;
 
+    // calculate the result
+    while (originalNumber != 0) {
+        remainder = originalNumber % 10;
+        result += pow(remainder, n);
+        originalNumber /= 10;
+    }
 
+    // check if number is an Armstrong number
+    if (result == number)
+        return 1; // number is Armstrong
+    else
+        return 0; // number is not Armstrong
+}
 
+int main() {
+    int number;
 
+    printf("Enter an integer: ");
+    scanf("%d", &number);
 
+    if (isArmstrong(number))
+        printf("%d is an Armstrong number.\n", number);
+    else
+        printf("%d is not an Armstrong number.\n", number);
 
-
-
-
-
-
-
-
-int main()
-{
-
+    return 0;
 }
