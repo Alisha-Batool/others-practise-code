84b2eeaad9b380aa0b2148d46f497ec4ce95fcbf
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2023-11-22 08:51:04 -0800

    make function

--- Alphabet.c
+++ Alphabet.c
@@ -1,27 +1,29 @@
 #include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-    //read in a sequence of character,one at a time
-    char input;
+int isAlphabet( char input){
     int vowels=0;
     int consonants=0;
     do
     {
-        /* code */
         // read in singal char value 
         scanf("%d\n", &input);
         // update counters
         if ( (input=='a' || input=='A') || (input=='e' || input=='E') || (input=='i' || input=='I') || (input=='o' || input=='O') || (input=='u' || input=='U'))
         {
             vowels++;
-            if((input>='a' && input<='z')|| (input>='A' && input<='Z'))
+            if((input>='a' && input<='z') || (input>='A' && input<='Z'))
             {
                 consonants++;
             }
         }
     }while (input='\n');
-    
+    return 0;
+}
+
+int main()
+{
+    //read in a sequence of character,one at a time
+    char input;
+    isAlphabet(input);
     printf("Number of vowels= %d \n Number of consonants= %d\n" );
     
     return 0;
