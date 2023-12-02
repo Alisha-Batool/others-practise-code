405211296672c7a8d88c4d9cbc808c2d90acab46
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2023-12-01 16:20:48 -0800

    write code

---
+++ array/VLA.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+void demoVla(){
+    int n;
+    printf("How many students are there?:");
+    scanf("%d", &n);
+    float marks[n];
+    //populate the array with user provided marks
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter marks for student %d:" , i);
+        scanf("%f" , &marks[i]);
+    }
+        //calculate the average marks
+        float sum=0;
+        for (int i = 0; i < n; i++)
+        {
+            sum= sum + marks[i];
+            //sum+=marks[i]
+        }
+        printf("Average score is %.2f", sum/n);
+        
+    
+    
+}
+int main(){
+    demoVla();
+    return 0;
+}
