2e49e4d7abd8a9378b14af7d7d7c5f22b9d518f2
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2024-01-08 11:51:21 -0800

    problem solved

--- array/transpose.c
+++ array/transpose.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define columns 3
-void transpose(int rows, int cols, int matrix[rows][cols], int result[cols][rows]);
-void transpose(int rows, int cols, int matrix[rows][cols], int result[cols][rows])
+#define rows 4
+#define cols 3
+void transpose( int matrix[rows][cols], int result[cols][rows]);
+void transpose( int matrix[rows][cols], int result[cols][rows])
 {
     for (int i = 0; i < rows; i++)
     {
@@ -14,11 +15,19 @@ void transpose(int rows, int cols, int matrix[rows][cols], int result[cols][rows
 }
 int main()
 {
-    int rows;
-    int cols;
+    
     int matrix[rows][cols]={{12,13,5},{5,7,21},{6,4,10},{5,5,9}};
+    printf("Before the transpose:\n");
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
     int result[cols][rows];
-    transpose(rows, cols, matrix, result);
+    transpose( matrix, result);
     printf("The transposed matrix is:\n");
     for (int i = 0; i < cols; i++)
     {
