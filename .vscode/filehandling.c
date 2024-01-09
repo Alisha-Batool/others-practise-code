74650729b2ce68217fe89c4436ab67d5c241a7d6
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2024-01-09 12:23:21 -0800

    edit code anf function call

--- .vscode/filehandling.c
+++ .vscode/filehandling.c
@@ -1,27 +1,27 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void demofilelinebyline()
 {
-    FILE *fp= fopen("Data.txt","w");
-    char line[][50]={{"First Line"},{"Second line"},{"Third line"}};
+    FILE *fp = fopen("Data.txt", "w");
+    char line[][50] = {{"First Line"}, {"Second line"}, {"Third line"}};
     for (int i = 0; i < 3; i++)
     {
-        fputs(line[i],fp);
-        fputs("\n",fp);
+        fputs(line[i], fp);
+        fputs("\n", fp);
     }
     fclose(fp);
 }
 
 void demoWritefilecharbychar()
 {
-    FILE *fp= fopen("Data_copy.txt","w");
-    char letters[]="this is a demo";
-    int i=0;
+    FILE *fp = fopen("Data_copy.txt", "w");
+    char letters[] = "this is a demo";
+    int i = 0;
     while (i < strlen(letters))
     {
-        fprintf(fp,"%s",letters[i++]);
+        fprintf(fp, "%s", letters[i++]);
     }
     fclose(fp);
 }
@@ -29,31 +29,39 @@ void demoWritefilecharbychar()
 void demoReadfilelinebyline()
 {
     char buffer[100];
-    FILE *fp=fopen ("Data.txt", "r");
-    if(fp==NULL)
+    FILE *fp = fopen("Data.txt", "r");
+    if (fp == NULL)
     {
         puts("Error,file does not open");
         return;
     }
-    int numcharread=0;
+    int numcharread = 0;
     do
     {
-        fgets(buffer,99,fp);
-        numcharread= strlen(buffer);
+        fgets(buffer, 99, fp);
+        numcharread = strlen(buffer);
         printf("%s", buffer);
-        buffer[0]= '\0';
+        buffer[0] = '\0';
     } while (numcharread != 0);
     fclose(fp);
 }
 
 void demoReadfilecharbychar()
 {
-    FILE *fp= fopen("Data_copy.txt","r");
+    FILE *fp = fopen("Data.txt", "r");
     char c;
     do
     {
-        c= fgetc(fp);
-        printf("%c",c);
+        c = fgetc(fp);
+        printf("%c", c);
     } while (c != EOF);
-    
+    fclose(fp);
+}
+int main()
+{
+    demofilelinebyline();
+    demoWritefilecharbychar();
+    demoReadfilecharbychar();
+    demoReadfilelinebyline();
+    return 0;
 }
